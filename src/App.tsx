fcc173d4ef81ab35d1c96394cd3361b40bfd3caa
chm132 <gusalstankvv@skuniv.ac.kr>
2024-01-12 22:19:35 +0900

    Banner 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Components/Home/Header';
+import Banner from './Components/Home/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Banner></Banner>
     </div>
   );
 }
--- src/Components/Home/Header.tsx
+++ src/Components/Home/Header.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="Header">
+      <p>Header</p>
+    </div>
+  );
 }
