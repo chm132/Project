045d575d6d7a422a741cad02c0eb022fd4bc79fb
chm132 <gusalstankvv@skuniv.ac.kr>
2024-01-14 16:53:53 +0900

    [feat]전역 폰트 변경

--- src/Components/Home/News/index.tsx
+++ src/Components/Home/News/index.tsx
@@ -1,46 +1,47 @@
-import { useState } from 'react'
-import Header from '../Header'
-import { NewsDataProps } from '../../../types/NewsDataProps'
+import { useState } from 'react';
+import Header from '../Header';
+import { NewsDataProps } from '../../../types/NewsDataProps';
 
 const NewsData = [
   {
     id: 1,
-    image: "/assets/News/news.jpeg",
-    part: "건강 상식",
-    title: "건강을 위해 반드시 지켜야 하는 식단!",
-    writer: "친절한 쩡이쌤"
+    image: '/assets/News/news.jpeg',
+    part: '건강 상식',
+    title: '건강을 위해 반드시 지켜야 하는 식단!',
+    writer: '친절한 쩡이쌤',
   },
   {
     id: 2,
-    image: "/assets/News/news.jpeg",
-    part: "건강 상식",
-    title: "건강을 위해 반드시 지켜야 하는 식단!",
-    writer: "친절한 쩡이쌤"
+    image: '/assets/News/news.jpeg',
+    part: '건강 상식',
+    title: '건강을 위해 반드시 지켜야 하는 식단!',
+    writer: '친절한 쩡이쌤',
   },
-]
+];
 
 const News = () => {
   const [newsData, setNewsData] = useState<NewsDataProps[]>(NewsData);
 
   return (
-    <div className=''>
+    <div className="">
       <Header partName="올래 생활 뉴스" />
-      <hr className='w-full h-[2px] bg-black my-5' />
+      <hr className="w-full h-[2px] bg-black my-5" />
       {newsData.map((news) => (
-        <div 
-          key={news.id}
-          className='flex gap-5'
-        >
-          <img src={news.image} alt={news.image} className='object-cover w-52 h-52'/>
-          <span className='font-bold'>
-            <p className='text-sm text-slate-500'>{news.part}</p>
-            <p className='py-5 text-lg'>{news.title}</p>
-            <p className='py-5 text-sm text-slate-500'>{news.writer}</p>
+        <div key={news.id} className="flex gap-5">
+          <img
+            src={news.image}
+            alt={news.image}
+            className="object-cover w-52 h-52"
+          />
+          <span className="font-bold">
+            <p className="text-sm text-slate-500">{news.part}</p>
+            <p className="py-5 text-lg">{news.title}</p>
+            <p className="py-5 text-sm text-slate-500">{news.writer}</p>
           </span>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
